rivet-gg/rivetkit

refactor(agentos): migrate integrations and publishing

examples/persistence/lifecycle-client.ts
@@ -1,21 +1,17 @@
-import { createClient } from "@rivet-dev/agentos/client";
-import type { registry } from "./server";
+import { vm } from "./client.js";
 
-const client = createClient<typeof registry>({
-	endpoint: "http://localhost:6420",
-});
+const conn = vm.connect();
+await conn.ready;
 
-const conn = client.vm.getOrCreate("my-agent").connect();
-
-conn.on("vmBooted", () => {
+conn.on("runtime.booted", () => {
 	console.log("VM is ready");
 });
 
-conn.on("vmShutdown", (payload) => {
+conn.on("runtime.shutdown", (payload) => {
 	console.log("VM shutdown reason:", payload.reason);
 	// reason: "sleep" | "destroy" | "error"
 });
 
-// The VM starts lazily on the first AgentOS action after wake, after the
+// The VM starts lazily on the first agentOS action after wake, after the
 // lifecycle subscriptions above are active.
-await conn.filesystem.exists("/");
+await conn.filesystem.exists({ path: "/" });

examples/processes/shell-events.ts
@@ -1,14 +1,13 @@
-import { createClient } from "@rivet-dev/agentos/client";
-import type { registry } from "./server";
+import { vm } from "./client.js";
 
-const client = createClient<typeof registry>({ endpoint: "http://localhost:6420" });
-const conn = client.vm.getOrCreate("my-agent").connect();
-const { shellId } = await conn.terminal.open();
+const conn = vm.connect();
+await conn.ready;
+const terminal = await conn.terminal.open({ options: { args: [], env: {} } });
 
-conn.on("shellData", (data) => {
-	if (data.shellId !== shellId) return;
-  // data.shellId: string
-  // data.data: Uint8Array
-  const text = new TextDecoder().decode(data.data);
-  process.stdout.write(text);
+conn.on("terminal.data", (data) => {
+	if (data.terminal.shellId !== terminal.shellId) return;
+	// data.terminal: { generation, shellId }
+	// data.data: Uint8Array
+	const text = new TextDecoder().decode(data.data);
+	process.stdout.write(text);
 });

rivet-dev/agentos

examples/persistence/lifecycle-client.ts
@@ -1,21 +1,17 @@
-import { createClient } from "@rivet-dev/agentos/client";
-import type { registry } from "./server";
+import { vm } from "./client.js";
 
-const client = createClient<typeof registry>({
-	endpoint: "http://localhost:6420",
-});
+const conn = vm.connect();
+await conn.ready;
 
-const conn = client.vm.getOrCreate("my-agent").connect();
-
-conn.on("vmBooted", () => {
+conn.on("vm.booted", () => {
 	console.log("VM is ready");
 });
 
-conn.on("vmShutdown", (payload) => {
+conn.on("vm.shutdown", (payload) => {
 	console.log("VM shutdown reason:", payload.reason);
 	// reason: "sleep" | "destroy" | "error"
 });
 
-// The VM starts lazily on the first AgentOS action after wake, after the
+// The VM starts lazily on the first agentOS action after wake, after the
 // lifecycle subscriptions above are active.
-await conn.filesystem.exists("/");
+await conn.filesystem.exists({ path: "/" });

examples/processes/shell-events.ts
@@ -1,14 +1,13 @@
-import { createClient } from "@rivet-dev/agentos/client";
-import type { registry } from "./server";
+import { vm } from "./client.js";
 
-const client = createClient<typeof registry>({ endpoint: "http://localhost:6420" });
-const conn = client.vm.getOrCreate("my-agent").connect();
-const { shellId } = await conn.terminal.open();
+const conn = vm.connect();
+await conn.ready;
+const terminal = await conn.terminal.open({ options: { args: [], env: {} } });
 
-conn.on("shellData", (data) => {
-	if (data.shellId !== shellId) return;
-  // data.shellId: string
-  // data.data: Uint8Array
-  const text = new TextDecoder().decode(data.data);
-  process.stdout.write(text);
+conn.on("terminal.output", (data) => {
+	if (data.terminal.shellId !== terminal.shellId) return;
+	// data.terminal: { generation, shellId }
+	// data.data: Uint8Array
+	const text = new TextDecoder().decode(data.data);
+	process.stdout.write(text);
 });